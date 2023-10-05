d809e917c90d22193d9bcd0321c45410c80f198a
Juan Pimentel <juandbpimentel@alu.ufc.br>
2023-10-05 10:01:19 -0300

    Refinamento da gameplay

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -61,7 +61,7 @@ func jump_processing():
 		velocity.y += grav
 		jumping = true
 		if velocity.y > 0:
-			if (Input.is_key_pressed(KEY_SPACE) || Input.is_joy_button_pressed(0,JOY_BUTTON_B) || Input.is_key_pressed(KEY_UP) || Input.is_key_pressed(KEY_W) || Input.is_action_just_pressed("jump")) :
+			if (Input.is_action_pressed("jump")) :
 				velocity.y = 100
 				gliding = true
 				falling = false
@@ -77,20 +77,23 @@ func jump_processing():
 			jumping = true
 
 func movement_and_animation():
+		if !((Input.is_action_pressed('move_left') && !Input.is_action_pressed('move_right')) 
+		|| (Input.is_action_pressed('move_right') && !Input.is_action_pressed('move_left'))):
+			velocity.x = 0
+			walking = false
+			return
+	
 		if Input.is_action_pressed('move_right'):
 			velocity.x = speed
 			$Sprite2D.flip_h = false
 			$CollisionPolygon2D.scale.x = 1
 			walking = true
 
-		elif Input.is_action_pressed('move_left'):
+		if Input.is_action_pressed('move_left'):
 			velocity.x = -speed
 			$Sprite2D.flip_h = true
 			$CollisionPolygon2D.scale.x = -1
 			walking = true
-		else:
-			velocity.x = 0
-			walking = false
 
 func setar_animacao():
 	var state = "idle"
@@ -109,8 +112,16 @@ func setar_animacao():
 
 
 func _on_area_2d_body_entered(body):
+	print(body)
 	if body.is_in_group("Inimigo"):
 		if (velocity.y > 0 && position.y < body.position.y):
 			body.queue_free()
 		else:
 			Global.dealDamagePlayer()
+
+
+func _on_area_2d_area_entered(area):
+	if area.is_in_group('Trap'):
+		get_tree().reload_current_scene()
+		Global.dealDamagePlayer()
+	pass # Replace with function body.
--- Scripts/void.gd
+++ Scripts/void.gd
@@ -1,8 +1,2 @@
 extends Area2D
 
-
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		get_tree().reload_current_scene()
-		Global.dealDamagePlayer()
-	pass # Replace with function body.
